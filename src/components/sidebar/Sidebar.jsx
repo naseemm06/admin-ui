cffd0e91202a00357103ae82c947edf3ec50170c
naseem umar masaid <naseem1masaid@gmail.com>
2024-04-08 13:07:03 +0700

    main

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,25 +1,51 @@
 import "./sidebar.scss";
+import { Link } from "react-router-dom";
+import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard';
 
 const Sidebar = () => {
-    return (<div className="sidebar">
-        <div className="top">
-            <div className="logo">Store</div>
-        </div>
-        <div className="center">
-            <ul>
-                <li>
-                    <span>list</span>
-                </li>
-                <li>
-                    <span>list</span>
-                </li>
-                <li>
-                    <span>list</span>
-                </li>
-            </ul>
-        </div>
-        <div className="bottom">Color options</div>
-    </div>);
+    return (
+        <div className="sidebar">
+            <div className="top">
+                <Link to="/">
+                    <span className="logo">Store</span>
+                </Link>
+            </div>
+            <hr />
+            <div className="center">
+                <ul>
+                    <p className="title">MAIN</p>
+                    <li>
+                        <span>Dashboard</span>
+                    </li>
+                    <p className="title">LIST</p>
+                    <Link to="/users">
+                        <li>
+
+                            <span>Users</span>
+                        </li>
+                    </Link>
+                    <Link to="/products">
+                        <li>
+                            <span>Products</span>
+                        </li>
+                    </Link>
+                    <li>
+                        <span>Orders</span>
+                    </li>
+                    <p className="title">USER</p>
+                    <li>
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <span>Logout</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="bottom">
+                <div className="colorOption"></div>
+                <div className="colorOption"></div>
+            </div>
+        </div>);
 
 }
 
