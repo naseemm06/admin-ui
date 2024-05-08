924794ca192d0ab427a31433e6301605ad4bea98
naseem umar masaid <naseem1masaid@gmail.com>
2024-05-08 20:36:13 +0700

    main

--- src/components/mydatatable/Mydatatable.jsx
+++ src/components/mydatatable/Mydatatable.jsx
@@ -1,80 +1,25 @@
 import "./mydatatable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import { Link } from "react-router-dom";
 
-import {
-    DataGrid
-}
-
-    from "@mui/x-data-grid";
-
-import {
-    Link
-}
-
-    from "react-router-dom";
-
-const columns = [{
-    field: "id", headerName: "ID", width: 70
-}
-
-    ,
-{
-    field: "cateName", headerName: "Name", width: 130
-}
-
-    ,
+const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "cateName", headerName: "Name", width: 130 },
 ];
 
-const rows = [{
-    id: 1, cateName: "Coffe"
-}
-
-    ,
-{
-    id: 2, cateName: "Non Coffe"
-}
-
-    ,
-{
-    id: 3, cateName: "Cake"
-}
-
-    ,
-{
-    id: 4, cateName: "Pastry"
-}
-
-    ,
-{
-    id: 5, cateName: "Cookie"
-}
-
-    ,
-{
-    id: 6, cateName: "Coffe"
-}
-
-    ,
-{
-    id: 7, cateName: "Non Coffe"
-}
-
-    ,
-{
-    id: 8, cateName: "Cake"
-}
-
-    ,
-{
-    id: 9, cateName: "Pastry"
-}
-
-    ,
-{
-    id: 10, cateName: "Cookie"
-}
-
-    ,
+const rows = [
+    { id: 1, cateName: "Coffee" },
+    { id: 2, cateName: "Non Coffee" },
+    { id: 3, cateName: "Cake" },
+    { id: 4, cateName: "Pastry" },
+    { id: 5, cateName: "Cookie" },
+    { id: 6, cateName: "Coffee" },
+    { id: 7, cateName: "Non Coffee" },
+    { id: 8, cateName: "Cake" },
+    { id: 9, cateName: "Pastry" },
+    { id: 10, cateName: "Cookie" },
 ];
+
 
 
 const Mydatatable = () => {
